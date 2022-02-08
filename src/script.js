314398efb322da7acec82044846d73d5405cc8f7
Jason Booth <hello@studiojasonbooth.com>
2022-02-08 14:19:20 +0000

    initial commit

---
+++ src/script.js
@@ -0,0 +1,10 @@
+import './style.css'
+
+const title = document.querySelector('h1')
+
+let mouseX = 0
+
+window.addEventListener('mousemove', (e) => {
+	mouseX = 100 + (e.clientX / window.innerWidth) * 800
+	title.style.fontVariationSettings = `"wght" ${mouseX.toFixed(2)}`
+})
